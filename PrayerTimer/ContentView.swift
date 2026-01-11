7d31b63813dfd82439636aad97e1e2a3e4d709a8
Recep Demir <recep.demir@powercoders.org>
2026-01-11 18:57:23 +0100

    Initial Commit

---
+++ PrayerTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PrayerTimer
+//
+//  Created by Recep Demir on 11.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
